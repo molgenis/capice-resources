f38306584a515ea46b946df50e30c33af4d16765
SietsmaRJ <robertjarik@hotmail.com>
2022-01-13 13:50:06 +0100

    - Updated exporter to directly export to VCF. - Updated global README - Updated train_data_creator to only include theory information about creating new train-test and validation datasets.

--- train_data_creator/test/test_exporter.py
+++ train_data_creator/test/test_exporter.py
@@ -22,14 +22,19 @@ class TestExporter(unittest.TestCase):
         exporter = Exporter(self.__test_dir__)
         some_data = pd.DataFrame(
             {
-                'foo': [1, 2, 3],
-                'bar': ['very', 'hot', 'fuzz']
+                '#CHROM': [1, 2, 3],
+                'POS': [100, 200, 300],
+                'REF': ['A', 'A', 'A'],
+                'ALT': ['C', 'C', 'C'],
+                'gene': ['very', 'hot', 'fuzz'],
+                'binarized_label': [1, 2, 3],
+                'sample_weight': [0.8, 0.9, 1.0]
             }
         )
         exporter.export_train_test_dataset(some_data)
-        self.assertIn('train_test.tsv.gz', os.listdir(self.__test_dir__))
+        self.assertIn('train_test.vcf.gz', os.listdir(self.__test_dir__))
         exporter.export_validation_dataset(some_data)
-        self.assertIn('validation.tsv.gz', os.listdir(self.__test_dir__))
+        self.assertIn('validation.vcf.gz', os.listdir(self.__test_dir__))
 
 
 if __name__ == '__main__':
