f50c4bf4176190c71e323eb7528c957e2a8921b0
SietsmaRJ <work.robertsietsma@gmail.com>
2022-06-09 15:28:54 +0200

    Possibly finalized. Unknown. Talk with Product Owner if more time should be invested in making even less trees by combining them.

--- utilities.py
+++ utilities.py
@@ -53,7 +53,17 @@ class LeafObtainer:
             self.current_path = []
             self.obtain_node = [0]
         print('Finished extracting trees from model.')
-        return self.path_collection
+
+        print('Starting conversion from paths to dataframe.')
+        result = self._convert_paths_list_to_dataframe(self.path_collection)
+        print('Finished conversion from paths to dataframe.')
+
+        print('Starting removal of duplicated entries within dataframe.')
+        before_drop = result.shape[0]
+        result = self._process_duplicates(result)
+        after_drop = result.shape[0]
+        print(f'Done. Dropped {before_drop - after_drop} entries.')
+        return result
 
     def _obtain_node_information(self, tree: dict, data):
         if tree['nodeid'] == self.obtain_node[-1]:
@@ -105,94 +115,85 @@ class LeafObtainer:
             raise ValueError('process_sample() has to be performed first!')
         return self.leaves
 
+    def get_paths(self):
+        """
+        Method to only obtain the paths for a given sample in process_sample().
 
-def process_duplicates(data: pd.DataFrame, timer=5):
-    data_copy = data.copy(deep=True)
-    cols = data_copy.columns.difference(['leaf', 'tree_number'], sort=False)
-    data_copy['uids'] = data_copy[cols].astype(str).agg('_'.join, axis=1)
-    uuids = data_copy['uids'].unique()
-    timer_bl = time.time()
-    current = 0
-    total = len(uuids)
-    skipped = 0
-    actually_processed_uids = []
-    for uid in uuids:
-        timer_il = time.time()
-        if timer_il - timer_bl > timer:
-            print(f'Processing: {current}/{total}')
-            timer_bl = time.time()
-        node_data = data_copy[data_copy['uids'] == uid]
-        if node_data.shape[0] < 2:
-            skipped += 1
-            current += 1
-        else:
-            actually_processed_uids.append(uid)
-            first_hit = node_data.index[0]
-            indexes_to_remove = list(node_data.index[1:])
-            sum_of_leaves = node_data['leaf'].sum()
-            data_copy.loc[first_hit, 'leaf'] = sum_of_leaves
-            data_copy.drop(index=indexes_to_remove, inplace=True)
-            current += 1
-    print(f'Done! processed {total} entries, skipped {skipped}.')
-    data_copy.drop(columns=['uids'], inplace=True)
-    return data_copy, actually_processed_uids
+        Raises
+        ------
+        ValueError
+            If get_leaf_scores() is accessed before process_sample() is performed.
 
+        Returns
+        -------
+        list
+            A list of all the full paths for given sample in process_sample().
+        """
+        if len(self.path_collection) == 0:
+            raise ValueError('process_sample() has to be performed first!')
+        return self.path_collection
 
-def convert_paths_list_to_dataframe(paths: list):
-    """
-    Converts a list of paths to a dataframe containing the feature_X, data_value_X,
-    required_value_X and the final column being leaf.
+    @staticmethod
+    def _convert_paths_list_to_dataframe(paths: list):
+        longest_path = 0
+        arrays = []
+        index = 1
 
-    Parameters
-    ----------
-    paths : list
-        The output of process_sample() of LeafObtainer().
+        for path in paths:
+            splitted_path = path.split('=>')
+            length_path = len(splitted_path)
+            if length_path > longest_path:
+                longest_path = length_path
+            features = []
+            entry_values = []
+            model_values = []
+            yes_nos = []
+            leaf = 0
+            for node in splitted_path:
+                if node.startswith('leaf'):
+                    leaf = float(node.split('leaf(')[1].split(')')[0])
+                else:
+                    features.append(node.split('(')[0])
+                    entry_values.append(node.split('(')[1].split('<')[0])
+                    model_values.append(node.split('<')[1].split('->')[0])
+                    yes_nos.append(node.split('->')[1].split(')')[0])
+            features_dict = {'tree_number': index}
+            for i in range(1, length_path):
+                features_dict[f'feature_{i}'] = features[i - 1]
+                features_dict[f'data_value_{i}'] = entry_values[i - 1]
+                features_dict[f'model_value_{i}'] = model_values[i - 1]
+                features_dict[f'yes_no_{i}'] = yes_nos[i - 1]
+            features_dict['leaf'] = leaf
+            arrays.append(pd.Series(features_dict).T)
+            index += 1
+        reindex = ['tree_number']
+        for i in range(1, longest_path):
+            reindex.append(f'feature_{i}')
+            reindex.append(f'data_value_{i}')
+            reindex.append(f'model_value_{i}')
+            reindex.append(f'yes_no_{i}')
+        reindex.append('leaf')
+        result = pd.concat(arrays, axis=1).reindex(reindex).T
+        return result
 
-    Returns
-    -------
-    pandas.DataFrame
-        The paths converted to a pandas dataframe.
-    """
-    longest_path = 0
-    arrays = []
-    index = 1
-
-    for path in paths:
-        splitted_path = path.split('=>')
-        length_path = len(splitted_path)
-        if length_path > longest_path:
-            longest_path = length_path
-        features = []
-        entry_values = []
-        model_values = []
-        yes_nos = []
-        leaf = 0
-        for node in splitted_path:
-            if node.startswith('leaf'):
-                leaf = float(node.split('leaf(')[1].split(')')[0])
-            else:
-                features.append(node.split('(')[0])
-                entry_values.append(node.split('(')[1].split('<')[0])
-                model_values.append(node.split('<')[1].split('->')[0])
-                yes_nos.append(node.split('->')[1].split(')')[0])
-        features_dict = {'tree_number': index}
-        for i in range(1, length_path):
-            features_dict[f'feature_{i}'] = features[i - 1]
-            features_dict[f'data_value_{i}'] = entry_values[i - 1]
-            features_dict[f'model_value_{i}'] = model_values[i - 1]
-            features_dict[f'yes_no_{i}'] = yes_nos[i - 1]
-        features_dict['leaf'] = leaf
-        arrays.append(pd.Series(features_dict).T)
-        index += 1
-    reindex = ['tree_number']
-    for i in range(1, longest_path):
-        reindex.append(f'feature_{i}')
-        reindex.append(f'data_value_{i}')
-        reindex.append(f'model_value_{i}')
-        reindex.append(f'yes_no_{i}')
-    reindex.append('leaf')
-    result = pd.concat(arrays, axis=1).reindex(reindex).T
-    return result
+    @staticmethod
+    def _process_duplicates(data: pd.DataFrame):
+        data_copy = data.copy(deep=True)
+        cols = data_copy.columns.difference(['leaf', 'tree_number'], sort=False)
+        data_copy['uids'] = data_copy[cols].astype(str).agg('_'.join, axis=1)
+        uuids = data_copy['uids'].unique()
+        actually_processed_uids = []
+        for uid in uuids:
+            node_data = data_copy[data_copy['uids'] == uid]
+            if node_data.shape[0] >= 2:
+                actually_processed_uids.append(uid)
+                first_hit = node_data.index[0]
+                indexes_to_remove = list(node_data.index[1:])
+                sum_of_leaves = node_data['leaf'].sum()
+                data_copy.loc[first_hit, 'leaf'] = sum_of_leaves
+                data_copy.drop(index=indexes_to_remove, inplace=True)
+        data_copy.drop(columns=['uids'], inplace=True)
+        return data_copy
 
 
 def calculate_capice_score(leaves: np.array):
@@ -209,4 +210,4 @@ def calculate_capice_score(leaves: np.array):
     float
         The supposed 1 - CAPICE score for all leaves for a single sample.
     """
-    return 1/(1+np.exp(leaves.sum()))
+    return 1 - (1/(1+np.exp(leaves.sum())))
