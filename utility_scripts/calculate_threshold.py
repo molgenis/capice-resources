972b450b679c75b149ab6605a7cfff0f2ede5d98
SietsmaRJ <work.robertsietsma@gmail.com>
2022-09-22 10:25:39 +0200

    Added error raised when a threshold could not be calculated.

--- utility_scripts/calculate_threshold.py
+++ utility_scripts/calculate_threshold.py
@@ -99,14 +99,18 @@ class ThresholdCalculator:
 
     def calculate_threshold(self):
         self.reset_threshold()
+        threshold_calculated = False
         for i in np.arange(0, 1, 0.01):
             self.data.loc[self.data['score'] >= i, 'calculated_threshold'] = 1
             recall = recall_score(y_true=self.data['binarized_label'], y_pred=self.data['calculated_threshold'])
             if 0.94 <= recall <= 0.96:
+                threshold_calculated = True
                 print(f'Threshold calculated, final threshold: {i}')
                 print(f'At recall score: {recall}')
                 break
             self.reset_threshold()
+        if not threshold_calculated:
+            raise ValueError("Unable to calculate threshold!")
 
     def reset_threshold(self):
         self.data['calculated_threshold'] = 0
