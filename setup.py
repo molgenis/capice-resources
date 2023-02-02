81c3194cf4b4df4842caf0eea5a43ec174386647
SietsmaRJ <work.robertsietsma@gmail.com>
2023-02-02 15:00:34 +0100

    chore: Update and equalize dependencies
    - Updated CAPICE-resources dependencies to latest and greatest direct dependencies that are also shared with CAPICE (other dependencies will be updated at a later date).
    - This is done together with CAPICE to equalize the dependency versions between the 2 repositories.

--- setup.py
+++ setup.py
@@ -22,10 +22,10 @@ setup(
     ],
     python_requires='>=3.9.*',
     install_requires=[
-        'pandas==1.5.2',
+        'pandas==1.5.3',
         'numpy==1.24.1',
         'matplotlib==3.5.1',
-        'scikit-learn==1.0.2',
+        'scikit-learn==1.2.1',
         'graphviz==0.19.1',
         'seaborn==0.12.1'
     ],
