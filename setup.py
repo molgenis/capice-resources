921700947f96bba93539fb07dbbc90d28a9ce50d
SietsmaRJ <work.robertsietsma@gmail.com>
2023-06-08 09:12:14 +0200

    fix: Python 3.11 support
    - Updated matplotlib 3.5.1 to 3.7.1 to support python3.11

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     install_requires=[
         'pandas==1.5.3',
         'numpy==1.24.1',
-        'matplotlib==3.5.1',
+        'matplotlib==3.7.1',
         'scikit-learn==1.2.1',
         'graphviz==0.19.1',
         'seaborn==0.12.1'
