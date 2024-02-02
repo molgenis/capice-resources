fd2c72d66552febc655e423630cdc14435ed3bbc
bartcharbon <b.charbon@umcg.nl>
2024-02-02 13:36:25 +0000

    remove extra directory layer

--- utility_scripts/create_poc.sh
+++ utility_scripts/create_poc.sh
@@ -14,7 +14,7 @@ errcho() { echo "$@"  1>&2; }
 main() {
 	digestCommandLine "$@"
 
-	mkdir -p "${WORKDIR}/poc/data/"
+	mkdir -p "${WORKDIR}/data/"
 	CAPICE="${WORKDIR}/capice"
 
   #FIXME:re-enable
@@ -64,24 +64,24 @@ digestCommandLine() {
 download(){
 	echo "downloading"
 	#FIXME: branchname in url
-	curl -Ls -o "${WORKDIR}/poc/data/train_input_raw.vcf.gz" "https://github.com/molgenis/capice/raw/feat/noGnomad/resources/train_input_raw.vcf.gz"
-	curl -Ls -o "${WORKDIR}/poc/data/predict_input_raw.vcf.gz" "https://github.com/molgenis/capice/raw/feat/noGnomad/resources/predict_input_raw.vcf.gz"
-	curl -Ls -o "${WORKDIR}/poc/data/breakends.vcf.gz" "https://github.com/molgenis/capice/raw/feat/noGnomad/tests/resources/breakends.vcf.gz"
-	curl -Ls -o "${WORKDIR}/poc/data/edge_cases.vcf.gz" "https://github.com/molgenis/capice/raw/feat/noGnomad/tests/resources/edge_cases.vcf.gz"
-	curl -Ls -o "${WORKDIR}/poc/data/symbolic_alleles.vcf.gz" "https://github.com/molgenis/capice/raw/feat/noGnomad/tests/resources/symbolic_alleles.vcf.gz"
+	curl -Ls -o "${WORKDIR}/data/train_input_raw.vcf.gz" "https://github.com/molgenis/capice/raw/feat/noGnomad/resources/train_input_raw.vcf.gz"
+	curl -Ls -o "${WORKDIR}/data/predict_input_raw.vcf.gz" "https://github.com/molgenis/capice/raw/feat/noGnomad/resources/predict_input_raw.vcf.gz"
+	curl -Ls -o "${WORKDIR}/data/breakends.vcf.gz" "https://github.com/molgenis/capice/raw/feat/noGnomad/tests/resources/breakends.vcf.gz"
+	curl -Ls -o "${WORKDIR}/data/edge_cases.vcf.gz" "https://github.com/molgenis/capice/raw/feat/noGnomad/tests/resources/edge_cases.vcf.gz"
+	curl -Ls -o "${WORKDIR}/data/symbolic_alleles.vcf.gz" "https://github.com/molgenis/capice/raw/feat/noGnomad/tests/resources/symbolic_alleles.vcf.gz"
 	echo "finished downloading"
 }
 
 run_vep(){
 	echo "running vep"
-	for file in ${WORKDIR}/poc/data/*.vcf.gz; do bash ${CAPICE_RESOURCES}/utility_scripts/slurm_run_vep.sh -p ${VIP_DIR} -i "${file}" -g -o "${file%.vcf.gz}_vep.vcf.gz"; done
-	 mv "${WORKDIR}/poc/data/train_input_raw_vep.vcf.gz" "${WORKDIR}/poc/data/train_input_annotated.vcf.gz"
-	 mv "${WORKDIR}/poc/data/predict_input_raw_vep.vcf.gz" "${WORKDIR}/poc/data/predict_input.vcf.gz"
+	for file in ${WORKDIR}/data/*.vcf.gz; do bash ${CAPICE_RESOURCES}/utility_scripts/slurm_run_vep.sh -p ${VIP_DIR} -i "${file}" -g -o "${file%.vcf.gz}_vep.vcf.gz"; done
+	 mv "${WORKDIR}/data/train_input_raw_vep.vcf.gz" "${WORKDIR}/data/train_input_annotated.vcf.gz"
+	 mv "${WORKDIR}/data/predict_input_raw_vep.vcf.gz" "${WORKDIR}/data/predict_input.vcf.gz"
 	echo "finished running vep"
 }
 
 conversion_tool(){
-	bash "${CAPICE}/scripts/convert_vep_vcf_to_tsv_capice.sh" -t -i "${WORKDIR}/poc/data/train_input_annotated.vcf.gz" -o "${WORKDIR}/poc/data/train_input_annotated.tsv.gz" -p "${BCFTOOLS_SIF}"
+	bash "${CAPICE}/scripts/convert_vep_vcf_to_tsv_capice.sh" -t -i "${WORKDIR}/data/train_input_annotated.vcf.gz" -o "${WORKDIR}/data/train_input_annotated.tsv.gz" -p "${BCFTOOLS_SIF}"
 }
 
 install_capice_resources(){
@@ -97,10 +97,10 @@ install_capice_resources(){
 
 process_vep(){
 	echo "running process_vep"
-	wget -P "${WORKDIR}/poc/data/" https://research.nhgri.nih.gov/CGD/download/txt/CGD.txt.gz
+	wget -P "${WORKDIR}/data/" https://research.nhgri.nih.gov/CGD/download/txt/CGD.txt.gz
 	module load Python/3.10.4-GCCcore-11.3.0-bare
 	source "${CAPICE_RESOURCES}/venv/bin/activate"
-	process-vep -g "${WORKDIR}/poc/data/CGD.txt.gz" -f "${CAPICE}/resources/train_features.json" -t "${WORKDIR}/poc/data/train_input_annotated.tsv.gz" -o "${WORKDIR}/poc/data/processed/"
+	process-vep -g "${WORKDIR}/data/CGD.txt.gz" -f "${CAPICE}/resources/train_features.json" -t "${WORKDIR}/data/train_input_annotated.tsv.gz" -o "${WORKDIR}/data/processed/"
 	deactivate
 	module purge
 	echo "finished process_vep"
@@ -123,8 +123,8 @@ create_poc_job() {
 	echo "writing train script"
 	echo "#!/bin/bash
 #SBATCH --job-name=capice_train
-#SBATCH --output=${WORKDIR}/poc/capice_train.log
-#SBATCH --error=${WORKDIR}/poc/capice_train.err
+#SBATCH --output=${WORKDIR}/capice_train.log
+#SBATCH --error=${WORKDIR}/capice_train.err
 #SBATCH --time=23:59:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=25gb
@@ -133,9 +133,9 @@ create_poc_job() {
 #SBATCH --get-user-env=L60
 module load Python/3.10.4-GCCcore-11.3.0-bare
 source ${CAPICE}/venv/bin/activate
-capice -v train -t 8 -i ${WORKDIR}/poc/data/processed/train_test.tsv.gz \
+capice -v train -t 8 -i ${WORKDIR}/data/processed/train_test.tsv.gz \
 -e ${CAPICE}/resources/train_features.json \
--o ${WORKDIR}/poc/xgb_booster_poc.ubj
+-o ${WORKDIR}/xgb_booster_poc.ubj
 " > "${WORKDIR}/train_poc.sh"
 	echo "finished writing train script"
 }
@@ -148,21 +148,21 @@ train(){
 
 vcf_to_tsv(){
 	echo "running vcf_to_tsv"
-	bash ${CAPICE}/scripts/convert_vep_vcf_to_tsv_capice.sh -p "${BCFTOOLS_SIF}" -t -i "${WORKDIR}/poc/data/predict_input.vcf.gz" -o "${WORKDIR}/poc/data/predict_input.tsv.gz"
-	bash ${CAPICE}/scripts/convert_vep_vcf_to_tsv_capice.sh -p "${BCFTOOLS_SIF}" -t -i "${WORKDIR}/poc/data/breakends_vep.vcf.gz" -o "${WORKDIR}/poc/data/breakends_vep.tsv.gz"
-	bash ${CAPICE}/scripts/convert_vep_vcf_to_tsv_capice.sh -p "${BCFTOOLS_SIF}" -t -i "${WORKDIR}/poc/data/edge_cases_vep.vcf.gz" -o "${WORKDIR}/poc/data/edge_cases_vep.tsv.gz"
-	bash ${CAPICE}/scripts/convert_vep_vcf_to_tsv_capice.sh -p "${BCFTOOLS_SIF}" -t -i "${WORKDIR}/poc/data/symbolic_alleles_vep.vcf.gz" -o "${WORKDIR}/poc/data/symbolic_alleles_vep.tsv.gz"
+	bash ${CAPICE}/scripts/convert_vep_vcf_to_tsv_capice.sh -p "${BCFTOOLS_SIF}" -t -i "${WORKDIR}/data/predict_input.vcf.gz" -o "${WORKDIR}/data/predict_input.tsv.gz"
+	bash ${CAPICE}/scripts/convert_vep_vcf_to_tsv_capice.sh -p "${BCFTOOLS_SIF}" -t -i "${WORKDIR}/data/breakends_vep.vcf.gz" -o "${WORKDIR}/data/breakends_vep.tsv.gz"
+	bash ${CAPICE}/scripts/convert_vep_vcf_to_tsv_capice.sh -p "${BCFTOOLS_SIF}" -t -i "${WORKDIR}/data/edge_cases_vep.vcf.gz" -o "${WORKDIR}/data/edge_cases_vep.tsv.gz"
+	bash ${CAPICE}/scripts/convert_vep_vcf_to_tsv_capice.sh -p "${BCFTOOLS_SIF}" -t -i "${WORKDIR}/data/symbolic_alleles_vep.vcf.gz" -o "${WORKDIR}/data/symbolic_alleles_vep.tsv.gz"
 	echo "finished vcf_to_tsv"
 }
 
 copy_to_capice(){
 	echo "running copy_to_capice"
-	cp "${WORKDIR}/poc/data/processed/train_test.tsv.gz" "${CAPICE}/resources/"
-	cp "${WORKDIR}/poc/data/predict_input.tsv.gz" "${CAPICE}/resources/"
-	cp "${WORKDIR}/poc/xgb_booster_poc.ubj" "${CAPICE}/tests/resources/"
-	cp "${WORKDIR}/poc/data/breakends_vep.tsv.gz" "${CAPICE}/tests/resources/"
-	cp "${WORKDIR}/poc/data/edge_cases_vep.tsv.gz" "${CAPICE}/tests/resources/"
-	cp "${WORKDIR}/poc/data/symbolic_alleles_vep.tsv.gz" "${CAPICE}/tests/resources/"
+	cp "${WORKDIR}/data/processed/train_test.tsv.gz" "${CAPICE}/resources/"
+	cp "${WORKDIR}/data/predict_input.tsv.gz" "${CAPICE}/resources/"
+	cp "${WORKDIR}/xgb_booster_poc.ubj" "${CAPICE}/tests/resources/"
+	cp "${WORKDIR}/data/breakends_vep.tsv.gz" "${CAPICE}/tests/resources/"
+	cp "${WORKDIR}/data/edge_cases_vep.tsv.gz" "${CAPICE}/tests/resources/"
+	cp "${WORKDIR}/data/symbolic_alleles_vep.tsv.gz" "${CAPICE}/tests/resources/"
 	echo "finished copy_to_capice"
 }
 
