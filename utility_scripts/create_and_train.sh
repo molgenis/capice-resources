b6e30c3e106f9e07f47594ce3f99854aef737c8c
bart.charbon <b.charbon@umcg.nl>
2024-03-27 12:32:27 +0100

    Add updated utility script - filter validationset for train test of previous model

--- utility_scripts/create_and_train.sh
+++ utility_scripts/create_and_train.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=capice_create_and_train
 #SBATCH --time=23:59:00
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=16gb
+#SBATCH --mem=32gb
 #SBATCH --nodes=1
 #SBATCH --export=NONE
 #SBATCH --get-user-env=L60
@@ -18,6 +18,8 @@ main() {
 	install_capice "${CAPICE_BRANCH}"
 	install_capice "${PROD_CAPICE_VERSION}"
 	create_train_data
+	download_prod_train_test
+	filter_validation
 	vep
 	postprocess
 	create_model_job
@@ -33,7 +35,7 @@ main() {
 digestCommandLine() {
 	readonly USAGE="
 	Usage:
-	create_and_train.sh -v <arg> -c <arg> -b <arg> -w <arg> -r <arg> -n <arg> -p <arg> -t <arg> -m <arg> 
+	create_and_train.sh -v <arg> -c <arg> -b <arg> -w <arg> -r <arg> -n <arg> -p <arg> -t <arg> -m <arg> -d <arg>
     v) path/to/vkgl_file
 		c) path/to/clinvar_file
 		b) path/to/bcftools_sif
@@ -43,9 +45,10 @@ digestCommandLine() {
 		p) path/to/vip
 		t) capice production tag
 		m) capice production model file name
+		d) capice traintest data file name
 	"
 
-	while getopts v:b:c:w:r:n:p:t:m: flag
+	while getopts v:b:c:w:r:n:p:t:m:d: flag
 	do
 		case "${flag}" in
 			v) VKGL_FILE=${OPTARG};;
@@ -57,13 +60,14 @@ digestCommandLine() {
 			p) VIP_DIR=${OPTARG};;
 			t) PROD_CAPICE_VERSION=${OPTARG};;
 			m) PROD_MODEL=${OPTARG};;
+			d) PROD_TRAIN_FILE=${OPTARG};;
 			\?)
 				errcho "Error: invalid option"
 				echo "${USAGE}"
 				exit 1;;
 		esac
 	done
-	
+
 	CAPICE="${WORKDIR}/capice/${CAPICE_BRANCH}"
 }
 
@@ -91,13 +95,30 @@ create_train_data() {
 	echo "finished running train-data-creator"
 }
 
+
+download_prod_train_test() {
+	mkdir -p ${WORKDIR}/validation/prod/
+	echo "downloading production train_test"
+	wget -P ${WORKDIR}/validation/prod/ "https://github.com/molgenis/capice/releases/download/${PROD_CAPICE_VERSION}/${PROD_TRAIN_FILE}"
+	echo "finished downloading production train_test"
+}
+
+filter_validation() {
+	ml HTSlib
+	gunzip -c ${WORKDIR}/validation/prod/${PROD_TRAIN_FILE} > ${WORKDIR}/validation/prod/previous_train.vcf
+	gunzip ${WORKDIR}/data/validation.vcf.gz
+	awk 'FNR==NR { a[$1_$2_$4_$5]; next } /^#/ || !($1_$2_$4_$5 in a)' ${WORKDIR}/validation/prod/previous_train.vcf ${WORKDIR}/data/validation.vcf | bgzip > ${WORKDIR}/data/filtered_validation.vcf.gz
+	rm ${WORKDIR}/data/validation.vcf
+	rm ${WORKDIR}/validation/prod/previous_train.vcf
+}
+
 vep() {
 	echo "running vep on train test"
 	bash ${CAPICE_RESOURCES}/utility_scripts/slurm_run_vep.sh -p ${VIP_DIR} -g -i ${WORKDIR}/data/train_test.vcf.gz -o ${WORKDIR}/data/train_test_vep.vcf.gz
 	bash ${CAPICE}/scripts/convert_vep_vcf_to_tsv_capice.sh -p ${BCFTOOLS_SIF} -t -i ${WORKDIR}/data/train_test_vep.vcf.gz -o ${WORKDIR}/data/train_test_vep.tsv.gz
 	echo "finished running vep on train test"
 	echo "running vep on validation"
-	bash ${CAPICE_RESOURCES}/utility_scripts/slurm_run_vep.sh -p ${VIP_DIR} -g -i ${WORKDIR}/data/validation.vcf.gz -o ${WORKDIR}/data/validation_vep.vcf.gz
+	bash ${CAPICE_RESOURCES}/utility_scripts/slurm_run_vep.sh -p ${VIP_DIR} -g -i ${WORKDIR}/data/filtered_validation.vcf.gz -o ${WORKDIR}/data/validation_vep.vcf.gz
 	bash ${CAPICE}/scripts/convert_vep_vcf_to_tsv_capice.sh -p ${BCFTOOLS_SIF} -t -i ${WORKDIR}/data/validation_vep.vcf.gz -o ${WORKDIR}/data/validation_vep.tsv.gz
 	echo "finished running vep on validation"
 }
@@ -143,7 +164,7 @@ train(){
 }
 
 download_model() {
-	echo "downlooading model"
+	echo "downloading model"
 	wget -P ${WORKDIR}/validation/ "https://github.com/molgenis/capice/releases/download/${PROD_CAPICE_VERSION}/${PROD_MODEL}"
 	echo "finished downloading model"
 }
@@ -159,6 +180,7 @@ run_capice(){
 
 compare_and_threshold(){
     echo "running compare_and_threshold"
+  module load Python/3.10.4-GCCcore-11.3.0-bare
 	source ${CAPICE_RESOURCES}/venv/bin/activate
 	compare-model-performance -a ${WORKDIR}/validation/new_validation_predicted.tsv.gz -l ${WORKDIR}/data/processed/validation.tsv.gz -b ${WORKDIR}/validation/prod_validation_predicted.tsv.gz -o ${WORKDIR}/validation/performance
 	threshold-calculator -v ${WORKDIR}/data/processed/validation.tsv.gz -s ${WORKDIR}/validation/new_validation_predicted.tsv.gz -o ${WORKDIR}/validation/threshold/
@@ -167,6 +189,7 @@ compare_and_threshold(){
 }
 
 explain(){
+  module load Python/3.10.4-GCCcore-11.3.0-bare
   source ${CAPICE}/venv/bin/activate
   mkdir -p ${WORKDIR}/explain/
   capice explain -i ${WORKDIR}/model/capice_model.ubj -o ${WORKDIR}/explain/new_explain.tsv.gz
@@ -175,6 +198,7 @@ explain(){
 }
 
 merge_rank(){
+  module load Python/3.10.4-GCCcore-11.3.0-bare
   source ${CAPICE_RESOURCES}/venv/bin/activate
   compare-model-features -a ${WORKDIR}/explain/new_explain.tsv.gz -b ${WORKDIR}/explain/${PROD_CAPICE_VERSION}_explain.tsv.gz -o ${WORKDIR}/explain/merged_grch38.tsv.gz
   deactivate
